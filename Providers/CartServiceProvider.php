b7091b7c0832eb00298dafbd81c8cef9de32ce4f
evgShes <mr.shes.mail@gmail.com>
2017-07-10 09:31:54 +0300

    change ServiceProvider, added publishes dependencies

--- Providers/CartServiceProvider.php
+++ Providers/CartServiceProvider.php
@@ -20,7 +20,11 @@ class CartServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'\..\migrations');
+//        $this->loadMigrationsFrom(__DIR__.'\..\migrations');
+        $this->publishes([
+            __DIR__.'/../migrations'=>database_path('migrations'),
+            __DIR__.'/../Model'=>app_path(),
+        ]);
     }
 
     /**
