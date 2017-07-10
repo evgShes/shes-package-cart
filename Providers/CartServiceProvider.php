f3d2c15b90cde68c24a1e0657810e4d11869d34a
evgShes <mr.shes.mail@gmail.com>
2017-07-10 11:53:19 +0300

    Fixed

--- Providers/CartServiceProvider.php
+++ Providers/CartServiceProvider.php
@@ -22,8 +22,8 @@ class CartServiceProvider extends ServiceProvider
     {
 //        $this->loadMigrationsFrom(__DIR__.'\..\migrations');
         $this->publishes([
-            __DIR__.'/../migrations'=>database_path('migrations'),
-            __DIR__.'/../Model'=>app_path(),
+            __DIR__.'/../migrations/'=>database_path('migrations'),
+            __DIR__.'/../Model/'=>app_path(),
         ]);
     }
 
